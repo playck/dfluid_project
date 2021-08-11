606bfe39fcf2dc36240f746035fe37863da4573b
playck <playck@naver.com>
2021-08-11 18:47:30 +0900

    [ADD] Card Component Style

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./Card.scss";
 
 const Card = ({ card }) => {
-  console.log(card);
   return (
     <li className="card">
       <img src={card.img} alt="프로필 사진" />
-      <div>{card.label}</div>
-      <div>{card.text}</div>
-      <span class="Learn-more">Learn more</span>
+      <div className="label">{card.label}</div>
+      <div className="text">{card.text}</div>
+      <span class="more">Learn more</span>
     </li>
   );
 };
