c57a6e035e950596b3f9a5137ae58afc1797eadf
playck <playck@naver.com>
2021-08-12 15:18:54 +0900

    [ADD] Add CardList shuffle func

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -3,30 +3,42 @@ import image1 from "../../image/image1.png";
 import image2 from "../../image/image2.png";
 import image3 from "../../image/image3.png";
 import Card from "../../Components/Card/Card";
-import "./Main.scss";
 import ImageContainer from "../../Components/ImageContainer/ImageContainer";
+import "./Main.scss";
 
 const Main = () => {
-  const [cards, setCards] = useState({
-    1: {
+  const [cards, setCards] = useState([
+    {
       id: "1",
       label: "Sed ut perspiciatis",
       text: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.",
       img: image1,
     },
-    2: {
+    {
       id: "2",
       label: "Lorem ipsum dolor",
       text: "Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.",
       img: image2,
     },
-    3: {
+    {
       id: "3",
       label: "Nemo enim ipsam",
       text: "Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor.",
       img: image3,
     },
-  });
+  ]);
+
+  const shuffle = (arr) => {
+    let j, x, i;
+    for (i = arr.length; i; i -= 1) {
+      j = Math.floor(Math.random() * i);
+      x = arr[i - 1];
+      arr[i - 1] = arr[j];
+      arr[j] = x;
+    }
+    return arr;
+  };
+  shuffle(cards);
 
   return (
     <>
@@ -34,8 +46,8 @@ const Main = () => {
         Snap photos and share like <br /> never before
       </header>
       <ul className="cardList">
-        {Object.keys(cards).map((key) => (
-          <Card key={key} card={cards[key]} />
+        {cards.map((card) => (
+          <Card key={card.id} card={card} />
         ))}
       </ul>
       <ImageContainer />
